621c8d8b8fe8e4c47132ee99ded1ea942e987d6c
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:52:09 -0300

    Extension method for dependency injection in the layer application

---
+++ GoodHambuerger.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using GoodHambuerger.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace GoodHambuerger.Application;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IItemService, ItemService>();
+        services.AddScoped<IOrderService, OrderService>();
+    }
+}
