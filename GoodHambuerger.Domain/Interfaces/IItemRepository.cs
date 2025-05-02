2c4c0e60000487cdd235867ea59d6493df54e862
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:42:35 -0300

    Add contract GetItemById in the IItemInterface

--- GoodHambuerger.Domain/Interfaces/IItemRepository.cs
+++ GoodHambuerger.Domain/Interfaces/IItemRepository.cs
@@ -5,6 +5,7 @@ namespace GoodHambuerger.Domain.Interfaces;
 public interface IItemRepository
 {
     IEnumerable<Item> GetItems();
+    Item GetItemById(int id);
     IEnumerable<Item> GetItemsSandwichesOnly();
     IEnumerable<Item> GetItemsExtraOnly();
 }
