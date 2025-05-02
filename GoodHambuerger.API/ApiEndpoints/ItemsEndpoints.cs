eb0c6612310d6ef7ecbb65d9ef1ec7383e577f55
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:54:31 -0300

    Add extension methods for MinimalApi endpoints

---
+++ GoodHambuerger.API/ApiEndpoints/ItemsEndpoints.cs
@@ -0,0 +1,37 @@
+﻿using GoodHambuerger.Application.Models;
+using GoodHambuerger.Application.Services;
+
+namespace GoodHambuerger.API.ApiEndpoints;
+
+public static class ItemsEndpoints
+{
+    public static void MapItemsEndpoints(this WebApplication app)
+    {
+        app.MapGet("/items", (IItemService service) =>
+        {
+            var result = service.ListItems();
+
+            return Results.Ok(result.Data);
+        })
+            .WithName("ListItems")
+            .WithOpenApi();
+
+        app.MapGet("/items/sandwiches", (IItemService service) =>
+        {
+            var result = service.ListSandwichesOnly();
+
+            return Results.Ok(result.Data);
+        })
+            .WithName("ListItemsSandwiches")
+            .WithOpenApi();
+
+        app.MapGet("/items/extras", (IItemService service) =>
+        {
+            var result = service.ListExtrasOnly();
+
+            return Results.Ok(result.Data);
+        })
+           .WithName("ListItemsExtras")
+           .WithOpenApi();
+    }
+}
---
+++ GoodHambuerger.API/ApiEndpoints/OrdersEntpoints.cs
@@ -0,0 +1,59 @@
+﻿using GoodHambuerger.Application.Models;
+using GoodHambuerger.Application.Services;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace GoodHambuerger.API.ApiEndpoints;
+
+public static class OrdersEntpoints
+{
+    public static void MapOrdersEndpoints(this WebApplication app)
+    {
+        app.MapPost("/orders", (OrderItemsInputModel model, IOrderService service) =>
+        {
+            var result = service.CreateOrder(model);
+
+            if (result.IsSuccess)
+            {
+                return Results.Created(string.Empty, result);
+            }
+
+            return Results.BadRequest(result);
+        })
+            .WithName("SendOrder")
+            .WithOpenApi();
+
+        app.MapGet("/orders", (IOrderService service) =>
+        {
+            var result = service.ListOrders();
+
+            return Results.Ok(result.Data);
+        })
+            .WithName("ListOrders")
+            .WithOpenApi();
+
+        app.MapPut("/orders", (OrderItemsUpdateInputModel model, IOrderService service) =>
+        {
+            var result = service.UpdateOrder(model);
+
+            if (result.IsSuccess)
+            {
+                return Results.NoContent();
+            }
+
+            return Results.BadRequest(result.Message);
+        });
+
+        app.MapDelete("/orders/{id:int}", (int id, IOrderService service) =>
+        {
+            var result = service.DeleteOrder(id);
+
+            if (result.IsSuccess)
+            {
+                return Results.NoContent();
+            }
+
+            return Results.NotFound(result.Message);
+        });
+            
+    }
+}
