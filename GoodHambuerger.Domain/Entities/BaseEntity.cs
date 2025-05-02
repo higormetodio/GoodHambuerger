d4340ac31452c5720b9c7f3db1a279d08c925023
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:43:54 -0300

    Created BaseEntity constructor

--- GoodHambuerger.Domain/Entities/BaseEntity.cs
+++ GoodHambuerger.Domain/Entities/BaseEntity.cs
@@ -2,6 +2,12 @@
 
 public abstract class BaseEntity
 {
+    protected BaseEntity(int id)
+    {
+        Id = id;
+        CreatedOn = DateTime.UtcNow;
+    }
+
     public int Id { get; private set; }
-    public DateTime CreatedOn { get; private set; } = DateTime.UtcNow;
+    public DateTime CreatedOn { get; private set; }
 }
