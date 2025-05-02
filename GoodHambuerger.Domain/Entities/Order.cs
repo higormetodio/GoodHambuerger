54227b2947939d130c5f7d9519f6970e12c00ae1
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:49:56 -0300

    Adjustments to the order entity constructor and add new methods CountItemsOrder and RemoveItems

--- GoodHambuerger.Domain/Entities/Order.cs
+++ GoodHambuerger.Domain/Entities/Order.cs
@@ -1,48 +1,62 @@
 ﻿using GoodHambuerger.Domain.Entities.Enums;
+using System.Text.Json.Serialization;
 
 namespace GoodHambuerger.Domain.Entities;
 
 public class Order : BaseEntity
 {
-    public Order(List<Item> items)
+    public Order(int id) : base(id)
     {
-        Items = items;
-        Discount();
+        Items = [];
     }
 
-    public decimal Total { get; private set; }
+    public decimal Total { get; private set; } = default;
+
+    [JsonIgnore]
     public List<Item> Items { get; private set; }
 
-    private void Discount()
+    public void AddItem(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void Discount()
     {
         Total = Items.Sum(item => item.Price);
 
         var haveSandwich = Items.Any(item => item.Type.Equals(ItemType.Sandwich));
         var haveFries = Items.Any(item => item.Name.Equals("Fries"));
-        var haveDrink = Items.Any(item => item.Name.Equals("Soft Drink"));
+        var haveDrink = Items.Any(item => item.Name.Equals("Soft drink"));
         
         if (haveSandwich && haveFries && haveDrink)
         {
             Total = Total - (Total * 0.2m);
+            return;
         }
 
         if (haveSandwich && haveDrink)
         {
             Total = Total - (Total * 0.15m);
+            return;
         }
 
         if (haveSandwich && haveFries)
         {
             Total = Total - (Total * 0.1m);
+            return;
         }
     }
 
-    public void UpdateOrder(List<Item> items)
+    public (int, int, int) CountItemsOrder()
     {
-        Items = items;
-        Discount();
+        var sandwiches = Items.Count(item => item.Type.Equals(ItemType.Sandwich));
+        var fries = Items.Count(item => item.Name.Equals("Fries"));
+        var drinks = Items.Count(item => item.Name.Equals("Soft drink"));
+        return (sandwiches, fries, drinks);
     }
 
-
-
+    public void RemoveItems()
+    {
+        Items.Clear();
+    }
 }
