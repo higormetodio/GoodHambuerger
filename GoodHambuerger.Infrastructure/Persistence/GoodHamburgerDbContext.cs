3370ed49403d44eb7743df64095b76ca4cc5123d
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:39:10 -0300

    Creating items seeds

--- GoodHambuerger.Infrastructure/Persistence/GoodHamburgerDbContext.cs
+++ GoodHambuerger.Infrastructure/Persistence/GoodHamburgerDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using GoodHambuerger.Domain.Entities;
+using GoodHambuerger.Domain.Entities.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace GoodHambuerger.Infrastructure.Persistence;
@@ -11,4 +12,15 @@ public class GoodHamburgerDbContext : DbContext
 
     public DbSet<Item> Items { get; set; }
     public DbSet<Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {       
+        builder.Entity<Item>().HasData(
+            new Item(1, "X Burger", ItemType.Sandwich, 5.00m),
+            new Item(2, "X Egg", ItemType.Sandwich, 4.50m),
+            new Item(3, "X Bacon", ItemType.Sandwich, 7.00m),
+            new Item(4, "Fries", ItemType.Extra, 2.00m),
+            new Item(5, "Soft drink", ItemType.Extra, 2.50m)
+            );
+    }
 }
