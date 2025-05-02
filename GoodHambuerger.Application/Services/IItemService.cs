d58fca69552e9c91a4c3dcc1f0aa512095015c9e
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:50:20 -0300

    Add ItemService

---
+++ GoodHambuerger.Application/Services/IItemService.cs
@@ -0,0 +1,10 @@
+﻿using GoodHambuerger.Application.Models;
+
+namespace GoodHambuerger.Application.Services;
+
+public interface IItemService
+{
+    ResultViewModel<IEnumerable<ItemViewModel>> ListItems();
+    ResultViewModel<IEnumerable<ItemViewModel>> ListSandwichesOnly();
+    ResultViewModel<IEnumerable<ItemViewModel>> ListExtrasOnly();
+}
---
+++ GoodHambuerger.Application/Services/ItemService.cs
@@ -0,0 +1,45 @@
+﻿using GoodHambuerger.Application.Models;
+using GoodHambuerger.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodHambuerger.Application.Services;
+
+public class ItemService : IItemService
+{
+    private readonly IItemRepository _itemRepository;
+
+    public ItemService(IItemRepository itemRepository)
+    {
+        _itemRepository = itemRepository;
+    }
+
+    public ResultViewModel<IEnumerable<ItemViewModel>> ListItems()
+    {
+        var items = _itemRepository.GetItems();
+
+        var model = items.Select(ItemViewModel.FromEntity).ToList();
+
+        return ResultViewModel<IEnumerable<ItemViewModel>>.Success(model);
+    }
+
+    public ResultViewModel<IEnumerable<ItemViewModel>> ListSandwichesOnly()
+    {
+        var items = _itemRepository.GetItemsSandwichesOnly();
+
+        var model = items.Select(ItemViewModel.FromEntity).ToList();
+
+        return ResultViewModel<IEnumerable<ItemViewModel>>.Success(model);
+    }
+    public ResultViewModel<IEnumerable<ItemViewModel>> ListExtrasOnly()
+    {
+        var items = _itemRepository.GetItemsExtraOnly();
+
+        var model = items.Select(ItemViewModel.FromEntity).ToList();
+
+        return ResultViewModel<IEnumerable<ItemViewModel>>.Success(model);
+    }    
+}
