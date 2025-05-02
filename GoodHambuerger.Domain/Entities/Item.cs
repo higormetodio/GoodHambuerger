1a0693e103af2fb608d9291e9398dc868abd5b77
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:45:34 -0300

    Add id to constructor and navigation property to order in the entity Item

--- GoodHambuerger.Domain/Entities/Item.cs
+++ GoodHambuerger.Domain/Entities/Item.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace GoodHambuerger.Domain.Entities;
 
 public class Item : BaseEntity
 {
-    public Item(string name, ItemType type, decimal price) : base()
+    public Item(int id, string name, ItemType type, decimal price) : base(id)
     {
         Name = name;
         Type = type;
@@ -19,4 +20,7 @@ public class Item : BaseEntity
     public string Name { get; private set; }
     public ItemType Type { get; private set; }
     public decimal Price { get; private set; }
+
+    [JsonIgnore]
+    public IEnumerable<Order> Orders { get; private set; }
 }
