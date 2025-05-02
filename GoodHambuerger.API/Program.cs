25c7176e15d446da0f2ed5be72ab77687a868e2a
Higor Metodio <higor.metodio@outlook.com>
2025-05-02 09:56:48 -0300

    Configure extension method AddApplication, ItemsEndpoints and OrderEndpoints. In-memory database configuration.

--- GoodHambuerger.API/Program.cs
+++ GoodHambuerger.API/Program.cs
@@ -1,11 +1,15 @@
+using GoodHambuerger.API.ApiEndpoints;
+using GoodHambuerger.Application;
 using GoodHambuerger.Infrastructure;
 using GoodHambuerger.Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructure();
+builder.Services.AddApplication();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,25 +27,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapOrdersEndpoints();
+app.MapItemsEndpoints();
 
-app.MapGet("/weatherforecast", () =>
+using (var scope = app.Services.CreateScope())
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var context = scope.ServiceProvider.GetRequiredService<GoodHamburgerDbContext>();
+    context.Database.EnsureCreated();
+}
 
 app.Run();
 
